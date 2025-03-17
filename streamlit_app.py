6fb0303e83321d400892e81b53c1601f2fb33d25
omaxim <maxim.oweyssi@gmail.com>
2025-03-17 11:58:26 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -140,6 +140,8 @@ def load_data(datayear):
     df['CZ 2030 Export CZK']        = USD_to_czk*df['CZ 2030 Export CZK'] 
     df['HS_ID']                 = df['HS_ID'].astype(str)
     df['HS_Lookup']              = df['HS_ID']+" - "+df['POPIS']
+    
+    st.info(str(GreenProducts.shape[0]) + " produktů načten z excelu, z toho" +str(df.shape[0])+" je IN")
     return df
 
 df = load_data(year)
