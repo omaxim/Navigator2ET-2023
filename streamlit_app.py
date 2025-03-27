f7b8c20e5cd718b995842064c798037fc1828a39
omaxim <maxim.oweyssi@gmail.com>
2025-03-28 00:15:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,7 @@ color_discrete_map = {
     'D01. Adaptace na dlouhodobé změny': '#000000',
     'C05. Kapaliny a plyny': '#000000',
     'D02. Živelné pohromy': '#000000',
+    'C01. Voda': '#000000',
 
     'A02c. Cyklistika a jednostopá': '#808080',
     'A03a. Snižování energetické náročnosti budov': '#94FFB5',
