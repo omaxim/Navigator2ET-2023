6a3f96009b2b31c6c78ba22dbd4eb623795ca710
omaxim <maxim.oweyssi@gmail.com>
2025-03-28 00:24:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,7 @@ color_discrete_map = {
     '2. Circularity': '#000000',
     'C01a. Zacházení s odpadními vodami': '#000000',
     'A06b. Distribuce a přenos paliv': '#000000',
+    'Trubky': '#000000',
 
     'A02c. Cyklistika a jednostopá': '#808080',
     'A03a. Snižování energetické náročnosti budov': '#94FFB5',
